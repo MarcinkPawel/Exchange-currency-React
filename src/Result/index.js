aec89e592fb976374851ce86c43dd531ad2ad880
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-02-01 22:55:06 +0100

    Revert "Result is fixed to hundredth"
    This reverts commit 8e7b1a5ff7761daafd44db7b3e34ab56aa2ec437.

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,14 +1,9 @@
 import "./style.css";
 
-const Result = ({ result }) => (
+const Result = ({ result, currency }) => (
   <p className="form__result">
-<<<<<<< HEAD
-    {result !== undefined && (
-      <span className="result">You will get {result}</span>
-    )}
-=======
     <span>{result !== undefined && `You will get ${result.toFixed(2)}`}</span>
->>>>>>> ce7f5ab44a64952bccd5087abe0004ebcc819153
+
   </p>
 );
 
