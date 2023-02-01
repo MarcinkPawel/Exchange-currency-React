b36255431eef4f9c56dee08979801597ed25c331
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-02-01 23:05:07 +0100

    Revert "Styling the result"
    This reverts commit e4d1109883c5928631bd8419c10bb710e4a3a631.

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Result = ({ result, currency }) => (
+const Result = ({ result }) => (
   <p className="form__result">
     <span>{result !== undefined && `You will get ${result.toFixed(2)}`}</span>
 
