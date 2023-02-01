c5046b91947c7963b0817ec12508f99d3e6ce290
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-02-01 22:57:04 +0100

    problem with version solved

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,6 @@
 import "./style.css";
 
-<<<<<<< HEAD
 const Header = ({header, subHeader}) => (
-=======
-const Header = ({ header, subHeader }) => (
->>>>>>> ce7f5ab44a64952bccd5087abe0004ebcc819153
   <header className="header">
     <h1 className="header__text">{header}</h1>
     <h2 className="header__text">{subHeader}</h2>
